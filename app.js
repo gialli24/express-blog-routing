2b06533b57121347d497bb4618f09422ef39227a
gialli24 <a.gialli2409@gmail.com>
2026-04-01 15:31:11 +0200

    add posts and images in public folder

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const postsRouter = require("./routers/posts");
 
 app.use("/posts", postsRouter);
 
+app.use(express.static('public'));
+
 app.listen(port, () => {
     console.log(`Listening on port ${port}`)
 });
